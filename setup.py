42b0c162eabf929940f69efa51592b0927b97a11
Simon Biggs <me@simonbiggs.net>
2018-11-05 21:41:00 +1100

    include some requirements for integrity

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="scriptedforms-experimental",
+    version="0.1.0",
+    author="Simon Biggs",
+    author_email="sbiggs@scriptedforms.com.au",
+    description="ScriptedForms.",
+    long_description=(
+        ""
+    ),
+    keywords=[],
+    packages=[
+        "scriptedforms"
+    ],
+    license='AGPL-3.0+',
+    python_requires='>=3.5',
+    classifiers=[],
+    url="https://scriptedforms.com.au",
+)
